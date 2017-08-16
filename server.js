230617488dc50c9ec29583f90d846682537070a7
salvator1808 <30893642+salvator1808@users.noreply.github.com>
2017-08-16 13:19:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/article-one', function (req, res) {
-  res.send('article one is here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 
